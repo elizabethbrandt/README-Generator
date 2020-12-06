d5cffd27bc4930378bd8931d80f96668045e9ca8
Elizabeth Brandt <ebrandt8@uw.edu>
2020-12-06 12:30:16 -0800

    starter files

---
+++ index.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// array of questions for user
+const questions = [
+
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    }
+
+];
+
+inquirer
+    .prompt(questions)
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
