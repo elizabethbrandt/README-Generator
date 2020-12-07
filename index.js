b9c2ea2a83b6e0f656aae083daab758aa4a794e8
Elizabeth Brandt <ebrandt8@uw.edu>
2020-12-06 20:09:13 -0800

    created questions and started markdown

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // array of questions for user
 const questions = [
 
     {
-        type: 'input',
-        message: '',
         name: 'title',
+        type: 'input',
+        message: 'What is the title of the project?'
     },
     {
+        name: 'description',
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What is the description of the project?'
     },
     {
+        name: 'installation',
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What are the steps required to install the project?',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'What are the instructions and/or examples for the use of the project?',
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'What license did you use for the project?',
+        choices: ['Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'GNU General Public License 2.0', 'Mozilla Public License', 'The Unlicense']
     }
 
 ];
-
-inquirer
-    .prompt(questions)
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -36,9 +45,11 @@ function init() {
         .prompt(questions)
         .then(response => {
 
-            const content = generateMarkdown(response);
+            console.log(response);
 
-        })
+            // const content = generateMarkdown(response);
+
+        });
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,64 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ${generateDescription(data.description)}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  ${generateInstallation(data.installation)}
+
+  ${generateUsage(data.usage)}
+
+  ${generateLicense(data.license)}
+  
+  `;
 }
+
+function generateDescription(description) {
+
+  return `## Description
+
+  ${description}`;
+  
+}
+
+function generateInstallation(installation) {
+
+  return `## Installation
+
+  ${installation}`;
+
+}
+
+function generateUsage(usage) {
+
+  return `## Usage
+
+  ${usage}`;
+
+}
+
+function generateLicense(license) {
+
+  return `## License
+
+  ${license}`;
+  
+}
+
+// console.log(generateMarkdown({
+//   title: "Testing",
+//   description: "Testing",
+//   tableOfContents: "One, Two, Three",
+//   installation: "NPM",
+//   usage: "see my video",
+//   license: "no idea"
+// }));
 
 module.exports = generateMarkdown;
